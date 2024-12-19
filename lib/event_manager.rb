f374f883274d11cc388426c7eed8eb991db0f424
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-18 20:07:46 -0800

    Complete iteration 2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,20 @@
 require 'csv'
+
+def clean_zip(zip)
+  zip.to_s.rjust(5, '0')[0..4]
+end
+
 puts 'Event Manager Initialized!'
 
-contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
 contents.each do |row|
   name = row[:first_name]
-  zip = row[:zipcode]
+  zip = clean_zip(row[:zipcode])
+
   puts "#{name} #{zip}"
 end
