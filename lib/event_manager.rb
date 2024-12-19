1212e7cd96edbf0cb909ca9ab7689f74b3113429
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-19 09:52:56 -0800

    Implement get_peak_hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -15,6 +16,35 @@ def clean_phone_number(number)
   else
     '0000000000'
   end
+end
+
+def format_time(string)
+  month = string.split('/')[0].rjust(2, '0')
+  day = string.split('/')[1].rjust(2, '0')
+  year = "20#{string.split('/')[2][0..1]}"
+  hour = string.split[1].split(':')[0].rjust(2, '0')
+  min = string.split[1].split(':')[1].rjust(2, '0')
+
+  Time.new("#{year}-#{month}-#{day} #{hour}:#{min}:00")
+end
+
+def count_hours(times)
+  times.each_with_object({}) do |cur, acc|
+    acc[cur.hour.to_s] = acc.has_key?(cur.hour.to_s) ? acc[cur.hour.to_s] += 1 : 1
+
+    acc
+  end
+end
+
+def get_peak_hours(regdate)
+  reg_times = []
+  regdate.each do |row|
+    reg_times << format_time(row)
+  end
+
+  hours = count_hours(reg_times)
+
+  hours.filter { |hour, count| count == hours.values.max }.keys
 end
 
 def legislators_by_zipcode(zip)
@@ -53,15 +83,20 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+regdate = contents.map { |row| row[:regdate] }
+peak_hours = get_peak_hours(regdate)
+
+p peak_hours
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   phone_number = clean_phone_number(row[:homephone])
-  legislators = legislators_by_zipcode(zipcode)
 
-  puts phone_number
-  form_letter = erb_template.result(binding)
+  # legislators = legislators_by_zipcode(zipcode)
 
-  save_thank_you_letter(id, form_letter)
+  # form_letter = erb_template.result(binding)
+
+  # save_thank_you_letter(id, form_letter)
 end
